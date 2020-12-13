d07abe580c645399dc41800a9d6a62323434353a
rahul411 <solankirahul411@gmail.com>
2020-12-13 00:41:09 -0800

    Updates

---
+++ precache-manifest.56ffdd1d2773ee50d32b297ea3f60c6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5a46e74a8ff0f3f5b1d77c25218922",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea46e4c10e037093d634",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a4631b46245b0ee416c4",
+    "url": "/static/js/2.da57f23e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.da57f23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea46e4c10e037093d634",
+    "url": "/static/js/main.f043c4f0.chunk.js"
+  },
+  {
+    "revision": "c6a23915f99d9f1c9f31",
+    "url": "/static/js/runtime-main.1b769beb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
